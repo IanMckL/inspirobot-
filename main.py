77e3fc25eb9de3c5d0f88d904ea420a5bc84e047
Ian <96411486+IanMckL@users.noreply.github.com>
2023-03-19 19:46:17 -0500

    Post image to instagram upon completion

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import os
+
 from blather import Blather
 from functions.joyImageGen import joyceImages
-
+from functions.joyceScraper import goodreadsquotes
+from functions.instagram import client
+dir = os.getcwd()
 # goodreadsquotes.grabber("https://www.goodreads.com/author/quotes/4114218.C_JoyBell_C_")
 blather = Blather()
 # blather.read('joyce.txt')
-# blather.save('joyc4.pt')
-blather.load('./models/joyc4.pt')
+# blather.save('joyc6.pt')
+blather.load(dir + '/joyc6.pt')
 
 res = ""
-
-while len(res) <10 or len(res) > 250:
+while len(res) < 45 or len(res) > 250:
     res = blather.write(' ')
+quoteText = '"' + res.strip() + '"'
+joyceImages.make_image(f'{quoteText}')
+client.make_post('generations/image.jpg', quoteText)
 
-joyceImages.make_image(f'{res}')
-print(f'C JoyBot Says: {res}')
