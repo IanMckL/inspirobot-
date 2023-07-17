5991a34e4f3e91e216ff2312037ec77216b7c543
ianlarsen <ian.mckenzie.l95@gmail.com>
2023-07-17 16:36:17 -0500

    Added customized ML model

--- functions/instagram/client.py
+++ functions/instagram/client.py
@@ -1,15 +1,43 @@
 import os
+import uuid
+
+from dotenv import load_dotenv
 from instagrapi import Client
-from dotenv import dotenv_values, load_dotenv
 
 load_dotenv()
 username = os.getenv('INSTAGRAM_USERNAME')
 password = os.getenv('INSTAGRAM_PASSWORD')
+client_session_id = str(uuid.uuid4())
 cl = Client()
+settings = {
+    "uuids": {
+        "phone_id": "be60e0a9-7201-5492-da04-4f2b85b3b592",
+        "uuid": "be60e0a9-7201-5492-da04-4f2b85b3b592",
+        "client_session_id": client_session_id,
+        "advertising_id": "1c413050-795d-f321-eeb4-4962ca0a731c",
+        "device_id": "android-f4f80570fe070b5f"
+    },
+    "cookies": {},
+    "last_login": 1674240519.7862692,
+    "device_settings": {
+        "cpu": "h1",
+        "dpi": "640dpi",
+        "model": "C6503",
+        "device": "C6503",
+        "resolution": "1794x1080",
+        "app_version": "117.0.0.28.123",
+        "manufacturer": "Sony",
+        "version_code": "164321834",
+        "android_release": "4.2.2",
+        "android_version": 23
+    },
+    "user_agent": "Opera/9.80 (Android 4.2.2; Linux; Opera Mobi/ADR-1210241554; U; en-us) Presto/2.11.355 Version/12.10"
+}
 
 
 def make_post(photo_path, text):
     try:
+        print(f'{username}')
         cl.login(f'{username}', f'{password}')
         cl.photo_upload(photo_path, f'{text}')
     except(RuntimeError, Exception):
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from threading import Thread
 from blather import Blather
@@ -5,20 +6,16 @@ from functions.joyImageGen import joyceImages
 from functions.joyceScraper import goodreadsquotes
 from functions.instagram import client
 from functions.flask import flaskEndpoint
-
-server_app = flaskEndpoint.init_flask()
+from functions.parroty.parroty import Parroty
+# server_app = flaskEndpoint.init_flask()
 directory = os.getcwd()
 # goodreadsquotes.grabber("https://www.goodreads.com/author/quotes/4114218.C_JoyBell_C_")
-blather = Blather()
-# blather.read('joyce.txt')
-# blather.save('joyc7.pt')
-blather.load(directory + '/models/joyc7.pt')
-
-res = ""
-while len(res) < 45 or len(res) > 250:
-    res = blather.write(' ')
+parroty = Parroty()
+parroty.load_model(directory + '/models/joyc7.pt')
+res = parroty.generate_text_from_model(' ')
 quoteText = '"' + res.strip() + '"'
 joyceImages.make_image(f'{quoteText}')
 
 client.make_post('generations/image.jpg', quoteText)
-# if using ngrok in a virtual environment, use os.chmod(executable, 755) on line 29 of flask_ngrok.py
+
+print(f'Posted to Instagram at {datetime.datetime.now()}')
